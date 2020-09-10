5f69bc5ac3a87af4d0ce57feb43db06ee8cb650e
Michael Corey Munn <coreymunn3@gmail.com>
2020-09-10 09:53:19 -0400

    Updates

---
+++ precache-manifest.b8660f8695a1b6e4e38453341d48160c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87779eecd05ffdfe0b54bd8712642df8",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "1a9675be3fb838874874",
+    "url": "/react-todo-app/static/css/main.90020670.chunk.css"
+  },
+  {
+    "revision": "09ed60a9a0131f43ae1f",
+    "url": "/react-todo-app/static/js/2.c2f83e63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.c2f83e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a9675be3fb838874874",
+    "url": "/react-todo-app/static/js/main.7cc93693.chunk.js"
+  },
+  {
+    "revision": "db6902fa5d1d97eedb42",
+    "url": "/react-todo-app/static/js/runtime-main.61c9d3d9.js"
+  }
+]);
